28aaf7745bb9acdfc5f80db4772c08b9f47c9150
denisdrabinka <denisdrabinka@mail.ru>
2019-11-14 23:13:02 +0300

    den

--- App1/App1/Class1.cs
+++ App1/App1/Class1.cs
@@ -11,11 +11,7 @@ namespace App1
         int user2;
         int user2B;
         int master;
-<<<<<<< HEAD
         int user2;
-=======
-        int user1;
->>>>>>> a8b04e5e970b611f5407bc0fbb834dc045d651b6
-
+        int den;
     }
 }
